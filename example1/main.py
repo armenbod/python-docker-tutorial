1480ff22cc3863a93f5e4f726f9dff163a275bdb
Armen Bodossian <armbodossian@gmail.com>
2023-10-26 17:36:09 +0100

    changing example1 to wikipedia top 25 articles since imdb forbidden and to practice BeautifulSoup

--- example1/main.py
+++ example1/main.py
@@ -4,7 +4,8 @@ from bs4 import BeautifulSoup
 
 # crawl IMDB Top 250 and randomly select a movie
 
-URL = 'http://www.imdb.com/chart/top'
+URL =  'https://en.wikipedia.org/wiki/Wikipedia:Top_25_Report'
+
 
 def main():
     response = requests.get(URL)
@@ -13,31 +14,33 @@ def main():
 
     # print(soup.prettify())
 
-    movietags = soup.select('td.titleColumn')
-    inner_movietags = soup.select('td.titleColumn a')
-    ratingtags = soup.select('td.posterColumn span[name=ir]')
 
-    def get_year(movie_tag):
-        moviesplit = movie_tag.text.split()
-        year = moviesplit[-1] # last item 
-        return year
+    #document.querySelector("#mw-content-text > div.mw-parser-output > table.wikitable > tbody > tr:nth-child(2) > td:nth-child(2) > a")
+    article = soup.select('tr')
+    title = soup.select('tr td:nth-child(2) a')
+    rank = soup.select('tr td:nth-child(1)')
 
-    years = [get_year(tag) for tag in movietags]
-    actors_list =[tag['title'] for tag in inner_movietags] # access attribute 'title'
-    titles = [tag.text for tag in inner_movietags]
-    ratings = [float(tag['data-value']) for tag in ratingtags] # access attribute 'data-value'
+    # def get_year(movie_tag):
+    #     moviesplit = movie_tag.text.split()
+    #     year = moviesplit[-1] # last item 
+    #     return year
 
-    n_movies = len(titles)
+    # years = [get_year(tag) for tag in movietags]
+    title_list =[tag['title'] for tag in title][1:] # access attribute 'title'
+    # titles = [tag.text for tag in inner_movietags]
+    rank_list = [x.text.replace('\n', '') for x in rank][1:]
+
+    n_titles = len(title_list)
 
     while(True):
-        idx = random.randrange(0, n_movies)
+        idx = random.randrange(0, n_titles)
         
-        print(f'{titles[idx]} {years[idx]}, Rating: {ratings[idx]:.1f}, Starring: {actors_list[idx]}')
+        print(f'{title_list[idx]}, {rank_list[idx]}')
 
         # comment the next line out to test user input with docker run -t -i
-        break
+        # break
     
-        user_input = input('Do you want another movie (y/[n])? ')
+        user_input = input('Do you want another wiki (y/[n])? ')
         if user_input != 'y':
             break
     
